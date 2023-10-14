33c5212c33ac1daa668e82ddca5a5bff1d7a93b8
Kristina Dzelebdzic <kristinaar94@gmail.com>
2023-10-14 16:33:30 -0700

    form added

--- src/pages/RequestsPage/RequestsPage.tsx
+++ src/pages/RequestsPage/RequestsPage.tsx
@@ -9,7 +9,6 @@ export const RequestsPage = () => {
 
       <div className="bg-gray-100 min-h-screen flex items-center justify-center">
         <div className="bg-white p-8 rounded shadow-md">
-          {/* <form onSubmit={(e) => handleSubmit(e)}> */}
           <form>
             <div className="mb-4">
               <label className="block text-sm font-medium text-gray-600">
@@ -17,9 +16,9 @@ export const RequestsPage = () => {
               </label>
               <input
                 type="text"
-                id="username"
-                name="username"
-                // value={formData.username}
+                id="name"
+                name="name"
+                // value={formData.name}
                 // onChange={(e) => handleFieldChange(e)}
                 className="mt-1 p-2 border border-gray-300 w-full"
                 required
@@ -34,7 +33,7 @@ export const RequestsPage = () => {
                 type="text"
                 id="last_name"
                 name="last_name"
-                // value={formData.username}
+                // value={formData.last_name}
                 // onChange={(e) => handleFieldChange(e)}
                 className="mt-1 p-2 border border-gray-300 w-full"
                 required
@@ -49,7 +48,7 @@ export const RequestsPage = () => {
                 type="email"
                 id="email"
                 name="email"
-                // value={formData.username}
+                // value={formData.email}
                 // onChange={(e) => handleFieldChange(e)}
                 className="mt-1 p-2 border border-gray-300 w-full"
                 required
@@ -64,7 +63,7 @@ export const RequestsPage = () => {
                 type="text"
                 id="date_from"
                 name="date_from"
-                // value={formData.username}
+                // value={formData.date_from}
                 // onChange={(e) => handleFieldChange(e)}
                 className="mt-1 p-2 border border-gray-300 w-full"
                 required
@@ -79,7 +78,7 @@ export const RequestsPage = () => {
                 type="text"
                 id="date_to"
                 name="date_to"
-                // value={formData.username}
+                // value={formData.date_to}
                 // onChange={(e) => handleFieldChange(e)}
                 className="mt-1 p-2 border border-gray-300 w-full"
                 required
@@ -94,28 +93,26 @@ export const RequestsPage = () => {
                 type="text"
                 id="status"
                 name="status"
-                // value={formData.username}
+                // value={formData.status}
                 // onChange={(e) => handleFieldChange(e)}
                 className="mt-1 p-2 border border-gray-300 w-full"
                 required
               />
             </div>
 
-            <div>
-              <button
-                type="submit"
-                className="bg-yellow-300 text-black p-3 font-light rounded mb-4"
-              >
-                Clear
-              </button>
-              <br />
-              <button
-                type="submit"
-                className="bg-yellow-300 text-black p-3 font-light rounded"
-              >
-                Search
-              </button>
-            </div>
+            <button
+              type="submit"
+              className="bg-yellow-300 text-black p-3 font-light rounded mb-4"
+            >
+              Clear
+            </button>
+            <br />
+            <button
+              type="submit"
+              className="bg-yellow-300 text-black p-3 font-light rounded"
+            >
+              Search
+            </button>
           </form>
         </div>
       </div>
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,14 +1,39 @@
-// export interface UserProfile {
-//   name: string;
-//   lastName: string;
-//   email: string;
-//   dateFrom: string;
-//   dateTo: string;
-//   status: string;
-// }
+export interface UserProfile {
+  name: string;
+  lastName: string;
+  email: string;
+  dateFrom: string;
+  dateTo: string;
+  status: string;
+}
 
-// const baseUrl = "http://localhost:5173";
+const baseUrl = "http://localhost:5173";
 
-// async function getUserProfile(): Promise<UserProfile> {
-//   const response = await fetch(`${baseUrl}`);
-// }
+async function getUserProfile(): Promise<UserProfile> {
+  const response = await fetch(`${baseUrl}/requests`);
+
+  if (!response.ok) throw new Error("Failed to fetch user profile");
+
+  return response.json();
+}
+
+async function updateUserProfile(
+  newData: UserProfile
+): Promise<{ message: string }> {
+  const response = await fetch(`${baseUrl}/requests`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newData),
+  });
+
+  if (!response.ok) throw new Error("Failed to update user profile");
+
+  return response.json();
+}
+
+export const apiService = {
+  getUserProfile,
+  updateUserProfile,
+};
